9f11c3c8e4354c8c9a532af8e6d2cba298cf2316
ncristiansa <ncristiansalinasandia@gmail.com>
2019-02-05 20:18:15 +0100

    camibos

--- adivina.py
+++ adivina.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, session,request
 import random
 app = Flask(__name__)
 
 numero = random.randint(0, 100)
 
 @app.route("/adivina", methods=['GET', 'POST'])
-def hello():
+def adivinanumero():
     if request.method=="POST":
         num1=request.form.get("num1")
         if int(num1)== int(numero):
@@ -14,6 +14,7 @@ def hello():
         	return "<p>No has acertado</p>"
     else:
         return '''<form action="/adivina" method="POST">
+                <h2>Numero aleatorio (0-100)</h2>
                 <label>Numero:</label></br>
                 <input type="number" name="num1"/><br/><br/>
                 <input type="submit"/>
